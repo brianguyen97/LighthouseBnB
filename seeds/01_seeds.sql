0f96e38ca5db59514b26f59d53da42b45ba847ce
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-06-08 19:00:49 -0600

    Create 01_seeds.sql

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (name, email, password)
+VALUES ('Billie Eilish', 'billieeilish@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Brian Nguyen', 'briannguyen@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
+('Stephen Curry', 'stephencurry@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
